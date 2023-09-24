10f1a51fc910ff1a0eba83360b6d5601fcb7c8db
AHPAHPAHP <109130010+AHPAHPAHP@users.noreply.github.com>
2023-09-24 21:35:43 +0900

    Add files via upload
    https://osxdaily.com/2018/10/09/fix-operation-not-permitted-terminal-error-macos/
    
    You have to change your settings before run the code

---
+++ backup.py
@@ -0,0 +1,66 @@
+#findind bluetooth connecting devices
+import os
+import plistlib
+import time
+import sqlite3
+import csv
+import re
+import traceback
+def makecopy():
+    if os.path.isdir('./copy_backup')==False:
+        os.mkdir('./copy_backup')
+    now = os.getcwd()+'/copy_backup'
+    
+    username=input("Enter the username: ")
+    path = "/Users/%s/Library/Application Support/MobileSync/"%username
+    os.chdir(path)
+    #print(now)
+    com = "cp -r Backup/ "+now
+    os.popen(com)
+    time.sleep(3)
+    os.chdir(now)
+
+def CheckPlist():
+    now = os.getcwd()
+
+    rootdir = now
+    for file in os.listdir(rootdir):
+        d = os.path.join(rootdir, file)
+        if os.path.isdir(d):
+            os.chdir(d+"/")
+            now2=os.listdir()
+            
+            plist_name = "Info.plist"
+            try:
+                with open(plist_name, 'rb') as f:
+                    plist_data = plistlib.load(f)
+                    #print(plist_data)
+                    #value_list = plist_data['\s']
+                    os.chdir('..')
+                    os.chdir('..')
+                with open("backupinfo.txt", "w") as f:
+                    #f.write("Mac address: ")
+                    valuable_data=['GUID','IMEI','IMEI2','Last Backup Date','MEID','Product Name','Product Type','Product Version',
+                                   'Serial Number','Target Identifier','Target Type','Unique Identifier']
+                    for device_mac in plist_data.keys(): 
+                        if device_mac in valuable_data:
+                        #f.write(device_mac+"\n") #mac 주소
+                            device_info = plist_data[device_mac]
+                            #for i in device_info.keys():  
+                            string = device_mac+" : "+str(device_info)
+                            f.write(string+"\n")
+                            f.write("\n")
+                print("Check The file, 'backupinfo.txt'")
+            except:
+                print("There is no devices in Mac")
+                print(traceback.format_exc())
+
+    
+
+
+
+def find_idevicebackup():
+    makecopy()
+    CheckPlist()
+
+find_idevicebackup()
