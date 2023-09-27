c366c270ccd286ba16869af072cbc9fd63c2ba2f
BeenyHail <119034536+BeenyHail@users.noreply.github.com>
2023-09-27 22:47:31 +0900

    Update Terminal_history.py

--- Terminal_history.py
+++ Terminal_history.py
@@ -7,17 +7,19 @@ import pandas as pd
 import time
 
 def make_copy(now, D_path):
-    if not(os.path.isdir("Copy_Terminal")):
-        os.mkdir("Copy_Terminal")
+    script_folder = os.path.dirname(os.path.abspath(__file__))
+    Copy_folder = os.path.join(script_folder, 'Copy_Terminal')
+    if not(os.path.isdir(Copy_folder)):
+        os.mkdir(os.path.join(script_folder, 'Copy_Terminal'))
     os.chdir(D_path)
     if os.path.exists(".bash_history"):
-        com = "cp .bash_history "+now+"/Copy_Terminal/.bash_history"
+        com = "cp .bash_history "+os.path.join(Copy_folder, '.bash_history')
         os.popen(com)
     if os.path.exists(".zsh_history"):
-        com = "cp .zsh_history "+now+"/Copy_Terminal/.zsh_history"
+        com = "cp .zsh_history "+os.path.join(Copy_folder, '.zsh_history')
         os.popen(com)
     time.sleep(1)
-    os.chdir(now)
+    os.chdir(script_folder)
 
 def Command_history():
     os.chdir("./Copy_Terminal/")
@@ -33,16 +35,21 @@ def Command_history():
     return terminal_history
 
 current_user = getpass.getuser() # Username
-now = os.getcwd() #현재 실행 경로
-D_path = "/Users/"+current_user+"/"
+D_path = os.path.expanduser(f"~/")
 #/Users/woobeenpark/Library/Application Support/com.apple.spotlight/com.apple.spotlight.Shortcuts.v3
 col = ["Command", "Shell"]
 terminal_history = []
+
+# 현재 스크립트 파일이 있는 폴더 경로
+script_folder = os.path.dirname(os.path.abspath(__file__))
+
 try:
-    make_copy(now, D_path)
+    make_copy(script_folder, D_path)
     terminal_history = Command_history()
     df = pd.DataFrame(terminal_history, columns=col)
-    os.chdir(now)
-    df.to_csv("./CSV_Terminal.csv", sep=',')
+    os.chdir(script_folder)
+    # CSV 파일로 저장
+    csv_file = os.path.join(script_folder, 'CSV_Terminal.csv')  # CSV 파일 경로 설정
+    df.to_csv(csv_file, sep=',')
 except FileNotFoundError:
     print("This is invalid account name. Try again.")
