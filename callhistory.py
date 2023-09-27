d6f579e9080316e58910fca754b4e6033933291c
Dabin-Joo <105407138+Dabin-Joo@users.noreply.github.com>
2023-09-27 11:34:46 +0200

    Update callhistory.py

--- callhistory.py
+++ callhistory.py
@@ -1,13 +1,9 @@
 import sqlite3
-from datetime import datetime
-import shutil
+import datetime
+import csv
 import os
-from openpyxl import Workbook
 import getpass
-
-# 파일 카피 불가
-# sudo를 사용해도 permission denied가 뜸
-# 한번 돌려봐 주세요...ㅠㅠㅠ 제 컴터가 문제가 아니길 바라면서도 돌아가길 바랍니다....
+import shutil
 
 def directory():
     # 스크립트 파일이 있는 디렉토리를 기준으로 대상 폴더를 설정합니다.
@@ -16,20 +12,84 @@ def directory():
     # 사용자 이름 가져오기
     current_user = getpass.getuser()
 
-    target_file = os.path.expanduser(f"~/Library/Application\ Support/CallHistoryDB/CallHistory.storedata-wal")
+    target_folder = os.path.expanduser(f"~/Library/Application Support/CallHistoryDB")
+    target_files = os.listdir(target_folder)
     
-    # 새로 생성할 폴더 이름
-    new_folder_name = 'destination_folder'
 
     # 현재 스크립트 파일이 있는 폴더 경로
     script_folder = os.path.dirname(os.path.abspath(__file__))
 
-    # 새로운 폴더 생성
-    new_folder_path = os.path.join(script_folder, new_folder_name)
-    os.makedirs(new_folder_path, exist_ok=True)
+    # 모든 파일을 복사하여 대상 폴더로 붙여넣기
+    for file in target_files:
+        source_file_path = os.path.join(target_folder, file)
+        destination_file_path = os.path.join(script_folder, file)
+        
+        # 파일을 복사하여 붙여넣기
+        try:
+            shutil.copy2(source_file_path, destination_file_path)
+            print(f'파일 복사 완료: {file}')
+        except Exception as e:
+            print(f'파일 복사 실패: {file}, 오류: {str(e)}')
 
-    # 파일 복사 (sudo를 사용하여 파일 복사)
-    os.system(f"cp {target_file} {new_folder_path}")
-
-# 스크립트 실행
 directory()
+
+def convert_coretime_to_readable(timestamp):
+     # Mac Absolute Time (CoreServices Timestamp)는 2001년 1월 1일 기준
+    base_date = datetime.datetime(2001, 1, 1)
+
+    # timedelta 객체를 사용해 초를 추가
+    dt_object = base_date + datetime.timedelta(seconds=timestamp)
+
+    # 원하는 포맷으로 datetime 객체를 문자열로 변환
+    readable_time = dt_object.strftime('%Y-%m-%d %H:%M:%S') + " (UTC+0)"
+    return readable_time
+
+conn = sqlite3.connect("CallHistory.storedata")
+cursor = conn.cursor()
+
+# CSV 파일 저장 경로
+csv_file = 'output.csv'
+
+# 특정 테이블 선택
+table_name = 'ZCALLRECORD'
+columns_to_read = ['ZADDRESS', 'ZISO_COUNTRY_CODE', 'ZDATE', 'ZDURATION']
+
+# SQL 쿼리 작성 (특정 테이블의 특정 열 읽기)
+query = f"SELECT {', '.join(columns_to_read)} FROM {table_name}"
+
+# 쿼리 실행
+cursor.execute(query)
+
+# 결과 가져오기
+rows = cursor.fetchall()
+
+# 결과 가져오기
+results = cursor.fetchall()
+
+
+# CSV 파일로 저장
+with open(csv_file, 'w', newline='') as csv_output:
+    csv_writer = csv.writer(csv_output)
+    
+    # 열 이름을 CSV 파일의 첫 행으로 쓰기
+    csv_writer.writerow(columns_to_read)
+    
+    for row in results:
+        # 타임스탬프 열의 값을 보기 좋은 형식으로 변환
+        timestamp_index = columns_to_read.index('ZDATE')
+        timestamp = row[timestamp_index]
+        formatted_timestamp = datetime.datetime.utcfromtimestamp(timestamp).strftime('%Y-%m-%d %H:%M:%S')
+        
+        # 타임스탬프 열의 값을 보기 좋은 형식으로 교체
+        row = list(row)
+        row[timestamp_index] = formatted_timestamp
+        
+        # 결과 행을 CSV 파일에 쓰기
+        csv_writer.writerow(row)
+
+
+print(f"데이터를 {csv_file}로 저장했습니다.")
+
+
+# 연결 종료
+conn.close()
