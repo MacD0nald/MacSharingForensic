f4786d7fb33ce32ac72fe63744004c2a01ff8225
BeenyHail <119034536+BeenyHail@users.noreply.github.com>
2023-09-29 19:01:57 +0900

    Update Terminal_history.py

--- Terminal_history.py
+++ Terminal_history.py
@@ -33,7 +33,7 @@ def Command_history():
                 for j in f.readlines():
                     terminal_history.append([str(j), "bash_shell"])
     return terminal_history
-if __name__ == "__main__":
+def Terminal():
     current_user = getpass.getuser() # Username
     D_path = os.path.expanduser(f"~/")
     col = ["Command", "Shell"]
@@ -42,13 +42,10 @@ if __name__ == "__main__":
     # 현재 스크립트 파일이 있는 폴더 경로
     script_folder = os.path.dirname(os.path.abspath(__file__))
     
-    try:
-        make_copy(script_folder, D_path)
-        terminal_history = Command_history()
-        df = pd.DataFrame(terminal_history, columns=col)
-        os.chdir(script_folder)
-        # CSV 파일로 저장
-        csv_file = os.path.join(script_folder, 'CSV_Terminal.csv')  # CSV 파일 경로 설정
-        df.to_csv(csv_file, sep=',')
-    except FileNotFoundError:
-        print("This is invalid account name. Try again.")
+    make_copy(script_folder, D_path)
+    terminal_history = Command_history()
+    df = pd.DataFrame(terminal_history, columns=col)
+    os.chdir(script_folder)
+    # CSV 파일로 저장
+    csv_file = os.path.join(script_folder, 'CSV_Terminal.csv')  # CSV 파일 경로 설정
+    df.to_csv(csv_file, sep=',')
